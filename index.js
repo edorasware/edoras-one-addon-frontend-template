6dd4ad02c9c82be0c9616f0f78cb91ba649029ae
Thomas Kaul <thomas.kaul@edorasware.com>
2017-08-04 15:13:28 +0200

    Add template files

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import './scss/main.scss';
-import module from './src';
+import module from './src/edoras-widget-star-rating.module';
+import config from './src/configuration';
+
+// register widget in view engine
+window.edoras.configuration.widgets.register({
+  config: config({}),
+  module: module.name,
+  name: 'edorasWidgetStarRating',
+  type: window.edoras.configuration.widgets.types.get().COMPONENT
+});
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'edoras-addon-star-rating.css'
+});
+
+module.exports = {
+  entry: {
+    widget: './index.js'
+  },
+  output: {
+    path: path.join(__dirname, '..', 'edoras-addon-star-rating',
+      'src', 'main', 'resources', 'com', 'edorasware', 'one', 'widgets'),
+    filename: 'edoras-addon-star-rating.js',
+    library: 'edorasWidgetStarRating',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            }, {
+              loader: 'sass-loader'
+            }
+          ],
+          // use style-loader in development
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractSass
+  ]
+};
