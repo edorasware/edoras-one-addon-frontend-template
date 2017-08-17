2c5232cda767cd2996fa975b6c7611b23f1fcf62
Thomas Kaul <thomas.kaul@edorasware.com>
2017-08-17 14:49:35 +0200

    Fix placeholder, add style

--- src/widget.module.js
+++ src/widget.module.js
@@ -5,5 +5,5 @@ import {{widgetNamePascalCase}}Service from './{{widgetNameParamCase}}.service';
 
 export default angular
   .module('{{widgetNameCamelCase}}Module', [])
-  .component('{{widgetNameCamelCase}}Rating', {{widgetNamePascalCase}}Component)
+  .component('{{widgetNameCamelCase}}', {{widgetNamePascalCase}}Component)
   .service('{{widgetNameCamelCase}}Service', {{widgetNamePascalCase}}Service);
