9b8e466b8f008cda3d3dc4f223e1b906d034dfad
Thomas Kaul <thomas.kaul@edorasware.com>
2017-08-18 08:51:35 +0200

    Start with generic example / remove star-rating

--- src/widget.controller.js
+++ src/widget.controller.js
@@ -7,8 +7,6 @@ export default class {{widgetNameCamelCase}}Controller {
     this.{{widgetNameCamelCase}}Service = {{widgetNameCamelCase}}Service;
 
     this.state = {};
-    this.state.data = null;
-    this.state.options = [1, 2, 3, 4, 5];
 
     this.$onInit = () => {
       this.initialize();
@@ -20,30 +18,18 @@ export default class {{widgetNameCamelCase}}Controller {
   }
 
   /**
-   * Load initial data
+   * Initialize
    * initialize :: undefined -> undefined
    */
   initialize() {
-    if (this.bindings.root.globalId) {
-      this.{{widgetNameCamelCase}}Service.loadRating(
-        this.bindings.root.globalId).then((response) => {
-        this.state.data = response.data;
-      });
-    }
   }
 
   /**
-   * Set rating (create and update)
-   * setRating :: undefined -> undefined
+   * Load data
+   * loadData :: undefined -> undefined
    */
-  setRating() {
-    this.{{widgetNameCamelCase}}Service.setRating(
-      this.bindings.root.globalId,
-      this.state.data.ownRating
-    ).then((response) => {
-      // Update state with most recent data
-      this.state.data = response.data;
-    });
+  loadData() {
+    console.log('Load data...');
   }
 
   /**
--- src/widget.service.js
+++ src/widget.service.js
@@ -7,24 +7,6 @@ export default class {{widgetNameCamelCase}}Service {
     this.$q = $q;
   }
 
-  /**
-   * Load rating via API
-   * loadRating :: string -> undefined
-   */
-  loadRating(globalId) {
-    return this.$http.get(`rating/${globalId}`);
-  }
-
-  /**
-   * Set rating via API
-   * setRating :: (string, integer) -> undefined
-   */
-  setRating(globalId, ownRating) {
-    return this.$http.post(`rating/${globalId}`, {
-      ownRating: ownRating
-    });
-  }
-
 }
 
 {{widgetNameCamelCase}}Service.$inject = [
